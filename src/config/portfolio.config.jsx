cb32b6f94efd90cc3270e29cce1ded8c78b266e2
jayzsh <jay.dnb@outlook.in>
2023-06-27 15:45:43 +0530

    Add portfolio projects array file

---
+++ src/config/portfolio.config.jsx
@@ -0,0 +1,14 @@
+export const projects = [
+  { index: 0, title: "Project 1" },
+  { index: 1, title: "Project 2" },
+  { index: 2, title: "Project 3" },
+  { index: 3, title: "Project 4" },
+  { index: 4, title: "Project 5" },
+  { index: 5, title: "Project 6" },
+  { index: 6, title: "Project 7" },
+  { index: 7, title: "Project 8" },
+  { index: 8, title: "Project 9" },
+  { index: 9, title: "Project 10" },
+  { index: 10, title: "Project 11" },
+  { index: 11, title: "Project 12" },
+];
