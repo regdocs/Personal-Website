4d361f691535849dbb03e7fa8c8e9ef039c67fda
jayzsh <jay.dnb@outlook.in>
2023-06-30 10:57:20 +0530

    Reorder languages: place odia with bangla *wink wink*

--- src/components/marq.jsx
+++ src/components/marq.jsx
@@ -1,9 +1,10 @@
 import Marquee from "react-fast-marquee";
+import "../styles/marquee.css";
 
 function Marq() {
   return (
     <div className="absolute overflow-hidden h-full w-full pointer-events-none">
-      <div className="text-white text-sm -rotate-90 left-4 top-1/2 absolute -translate-x-1/2 w-[100vh] pointer-events-auto">
+      <div className="text-white text-sm marquee-cust -rotate-90 left-4 top-1/2 absolute -translate-x-1/2 w-[100vh] pointer-events-auto">
         <Marquee
           delay={3}
           pauseOnHover
@@ -11,15 +12,14 @@ function Marq() {
           className="hover:cursor-default"
         >
           Привет • Bonjour • 你好 • مرحبًا • Hujambo • 안녕 • नमस्ते • Ciao •
-          নমস্কার • Merhaba • やあ • שלום • Γειά σου • Բարեւ • Olá • ਸਤ ਸ੍ਰੀ
-          ਅਕਾਲ • Xin chào • سلام • Kumusta • வணக்கம் • హలో • مهلا • Привіт • ಹಲೋ
-          • สวัสดี • Salut • ສະບາຍດີ • ਨਮਸਕਾਰ • مرحبا • Բարեւանդ • ഹലോ •
-          ନମସ୍କାର • سلام • پہلوان • ជំរាបសួរ • ਸਤ ਸ੍ਰੀ ਅਕਾਲ • Привіт • ಹಲೋ •
-          สวัสดี • Salut • ສະບາຍດີ • ਨਮਸਕਾਰ • مرحبا • Բարեւանդ • ഹലോ • ନମସ୍କାର
-          • سلام • پہلوان • ជំរាបសួរ
+          Merhaba • ନମସ୍କାର • নমস্কার • やあ • שלום • Γειά σου • Բարեւ • Olá •
+          ਸਤ ਸ੍ਰੀ ਅਕਾਲ • Xin chào • سلام • Kumusta • வணக்கம் • హలో • مهلا •
+          Привіт • ಹಲೋ • สวัสดี • Salut • ສະບາຍດີ • ਨਮਸਕਾਰ • مرحبا • Բարեւանդ •
+          ഹലോ • سلام • پہلوان • ជំរាបសួរ • ਸਤ ਸ੍ਰੀ ਅਕਾਲ • Привіт • ಹಲೋ • สวัสดี
+          • Salut • ສະບາຍດີ • ਨਮਸਕਾਰ • مرحبا • Բարեւանդ • ഹലോ • ନମସ୍କାର • سلام
+          • پہلوان • ជំរាបសួរ
           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
         </Marquee>
-        <div className="pointer-events-none absolute top-0 marquee-overlay w-full h-full z-10" />
       </div>
     </div>
   );
