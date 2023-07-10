f400166a5cd410c3fd2d578f088bbc6d3c574302
jayzsh <jay.dnb@outlook.in>
2023-07-10 22:31:24 +0530

    Prep for deployment

--- README.md
+++
@@ -1,2 +0,0 @@
-# Personal-Website
-My personal website using Vite + React.js + Tailwind + Redux
--- src/context/dotmap.context.jsx
+++
@@ -1,3 +0,0 @@
-import { createContext } from "react";
-
-export const DotmapContext = createContext([-1, -1]);
